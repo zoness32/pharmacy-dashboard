e8511b2675f7ccbf90d3d67e742294dd0d917a37
Taylor Eernisse <tayloreernisse@gmail.com>
2023-12-25 20:29:32 -0500

    display visits in an accordion, and sort visits by date descending

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,32 +1,18 @@
 import prisma from './db';
 
-export async function fetchPatients() {
-    try {
-        return await prisma.patient.findMany({});
-    } catch (error) {
-        console.error('Database Error:', error);
-        throw new Error('Failed to fetch the latest invoices.');
-    }
-}
-
-export async function fetchFirstPatient() {
-    try {
-        return await prisma.patient.findFirst({});
-    } catch (error) {
-        console.error('Database Error:', error);
-        throw new Error('Failed to fetch the latest patient info.');
-    }
-}
-
 export async function fetchPatientAndVisits() {
     try {
         return await prisma.patient.findFirst({
             include: {
-                visits: true
+                visits: {
+                    orderBy: {
+                        visit_time: 'desc'
+                    }
+                }
             }
         });
     } catch (error) {
         console.error('Database Error:', error);
-        throw new Error('Failed to fetch the latest visits for patient');
+        throw new Error('Failed to fetch the patient');
     }
 }
--- app/ui/patient-dashboard/visit-table.tsx
+++ app/ui/patient-dashboard/visit-table.tsx
@@ -1,5 +1,6 @@
 import {Visit} from "@prisma/client";
 import dayjs from "dayjs";
+import {Accordion, AccordionContent, AccordionPanel, AccordionTitle} from "flowbite-react";
 
 interface VisitTableProps {
     visits: Array<Visit>;
@@ -7,12 +8,28 @@ interface VisitTableProps {
 
 export default async function VisitTable({visits}: VisitTableProps) {
     return (
-        <div className="flex flex-col">
+        <Accordion>
             {
                 visits.map(visit => {
-                    return <span key={visit.id}>{dayjs(visit.visit_time).format('MM/DD/YYYY')}</span>
+                    return <AccordionPanel key={visit.id}>
+                        <AccordionTitle>{dayjs(visit.visit_time).format("MM/DD/YYYY")} by {visit.administering_nurse}</AccordionTitle>
+                        <AccordionContent>
+                            <div className="flex flex-col">
+                                <span><strong>Administration Location: </strong>{visit.administration_location}</span>
+                                <span><strong>Address: </strong>{visit.location_address} {visit.location_city}, {visit.location_state} {visit.location_zipcode}</span>
+                                <span><strong>Administering Nurse: </strong>{visit.administering_nurse}</span>
+                                <span><strong>Visit Duration: </strong>{visit.visit_duration_seconds}</span>
+                                <span><strong>Medication: </strong>{visit.medication}</span>
+                                <span><strong>Biometric Info</strong></span>
+                                <span><strong>Blood Pressure: </strong>{visit.blood_pressure}</span>
+                                <span><strong>Heart Rate: </strong>{visit.heartrate_bpm}</span>
+                                <span><strong>Medication Tolerance: </strong>{visit.medication_tolerance}</span>
+                                <span><strong>Pain Level: </strong>{visit.pain_level}</span>
+                            </div>
+                        </AccordionContent>
+                    </AccordionPanel>
                 })
             }
-        </div>
+        </Accordion>
     )
 }
