4b9cd220d4c9ef0abf53adc5b423f125f15d8bd7
Taylor Eernisse <tayloreernisse@gmail.com>
2023-12-25 19:56:40 -0500

    added key prop to span to fix Vercel build; these errors don't appear locally so... maybe address that in some way eventually?

--- app/ui/patient-dashboard/visit-table.tsx
+++ app/ui/patient-dashboard/visit-table.tsx
@@ -10,7 +10,7 @@ export default async function VisitTable({visits}: VisitTableProps) {
         <div className="flex flex-col">
             {
                 visits.map(visit => {
-                    return <span>{dayjs(visit.visit_time).format('MM/DD/YYYY')}</span>
+                    return <span key={visit.id}>{dayjs(visit.visit_time).format('MM/DD/YYYY')}</span>
                 })
             }
         </div>
