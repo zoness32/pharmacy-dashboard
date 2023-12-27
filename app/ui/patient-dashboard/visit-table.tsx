540f97a98daf6bc80354427f5cf33cccccee99a9
Taylor Eernisse <tayloreernisse@gmail.com>
2023-12-27 09:10:11 -0500

    switched visits display to a table

--- app/ui/patient-dashboard/visit-table.tsx
+++ app/ui/patient-dashboard/visit-table.tsx
@@ -1,35 +1,57 @@
 import {Visit} from "@prisma/client";
 import dayjs from "dayjs";
-import {Accordion, AccordionContent, AccordionPanel, AccordionTitle} from "flowbite-react";
+import duration from "dayjs/plugin/duration"
+import {Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow} from "flowbite-react";
 
 interface VisitTableProps {
     visits: Array<Visit>;
 }
 
 export default async function VisitTable({visits}: VisitTableProps) {
+    dayjs.extend(duration);
+
     return (
-        <Accordion>
-            {
-                visits.map(visit => {
-                    return <AccordionPanel key={visit.id}>
-                        <AccordionTitle>{dayjs(visit.visit_time).format("MM/DD/YYYY")} by {visit.administering_nurse}</AccordionTitle>
-                        <AccordionContent>
-                            <div className="flex flex-col">
-                                <span><strong>Administration Location: </strong>{visit.administration_location}</span>
-                                <span><strong>Address: </strong>{visit.location_address} {visit.location_city}, {visit.location_state} {visit.location_zipcode}</span>
-                                <span><strong>Administering Nurse: </strong>{visit.administering_nurse}</span>
-                                <span><strong>Visit Duration: </strong>{visit.visit_duration_seconds}</span>
-                                <span><strong>Medication: </strong>{visit.medication}</span>
-                                <span><strong>Biometric Info</strong></span>
-                                <span><strong>Blood Pressure: </strong>{visit.blood_pressure}</span>
-                                <span><strong>Heart Rate: </strong>{visit.heartrate_bpm}</span>
-                                <span><strong>Medication Tolerance: </strong>{visit.medication_tolerance}</span>
-                                <span><strong>Pain Level: </strong>{visit.pain_level}</span>
-                            </div>
-                        </AccordionContent>
-                    </AccordionPanel>
-                })
-            }
-        </Accordion>
+        <div className='overflow-x-auto'>
+            <Table striped>
+                <TableHead>
+                    <TableHeadCell>Visit Date</TableHeadCell>
+                    <TableHeadCell>Administering Nurse</TableHeadCell>
+                    <TableHeadCell>Administration Location</TableHeadCell>
+                    <TableHeadCell>Address</TableHeadCell>
+                    <TableHeadCell>City</TableHeadCell>
+                    <TableHeadCell>State</TableHeadCell>
+                    <TableHeadCell>Zip</TableHeadCell>
+                    <TableHeadCell>Visit Duration</TableHeadCell>
+                    <TableHeadCell>Medication</TableHeadCell>
+                    <TableHeadCell>Medication Tolerance</TableHeadCell>
+                    <TableHeadCell>Blood Pressure</TableHeadCell>
+                    <TableHeadCell>Heart Rate (bpm)</TableHeadCell>
+                    <TableHeadCell>Pain Level</TableHeadCell>
+                </TableHead>
+                <TableBody className='divide-y'>
+                    {
+                        visits.map(visit => {
+                            return (
+                                <TableRow key={visit.id}>
+                                    <TableCell>{dayjs(visit.visit_time).format('MM/DD/YYYY hh:mm:ss')}</TableCell>
+                                    <TableCell>{visit.administering_nurse}</TableCell>
+                                    <TableCell>{visit.administration_location}</TableCell>
+                                    <TableCell>{visit.location_address}</TableCell>
+                                    <TableCell>{visit.location_city}</TableCell>
+                                    <TableCell>{visit.location_state}</TableCell>
+                                    <TableCell>{visit.location_zipcode}</TableCell>
+                                    <TableCell>{dayjs(visit.visit_duration_seconds * 1000).toISOString().slice(11, 19)}</TableCell>
+                                    <TableCell>{visit.medication}</TableCell>
+                                    <TableCell>{visit.medication_tolerance}</TableCell>
+                                    <TableCell>{visit.blood_pressure}</TableCell>
+                                    <TableCell>{visit.heartrate_bpm}</TableCell>
+                                    <TableCell>{visit.pain_level}</TableCell>
+                                </TableRow>
+                            )
+                        })
+                    }
+                </TableBody>
+            </Table>
+        </div>
     )
 }
