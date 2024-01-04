714fd57693794819a44b7245b61486a9ae7a0485
Taylor Eernisse <tayloreernisse@gmail.com>
2024-01-04 17:49:07 -0500

    fixed padding issue

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children} : {children: React.ReactNode}) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-blue-300">
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{ children }</div>
+      <div className="flex-grow p-6 md:overflow-y-auto">{ children }</div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import PatientsTable from "@/app/ui/patients/patients-table";
 export default async function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col p-6 md:p-12 bg-blue-300">
+    <main className="flex min-h-screen flex-col p-6 bg-blue-300">
       <h1 className={`mb-4 text-xl md:text-2xl text-black`}>Patients</h1>
       <PatientsTable />
     </main>
